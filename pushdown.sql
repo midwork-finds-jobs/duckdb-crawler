74870ca0abf70186e976148b74d3bad474f2c051
Onni Hakala <onni@flaky.build>
2026-01-18 06:17:13 +0200

    Use htmlpath() to extract job IDs directly

--- pushdown.sql
+++ pushdown.sql
@@ -8,35 +8,30 @@
 
 LOAD 'build/release/extension/crawler/crawler.duckdb_extension';
 
--- Create or replace jobs table for crawl results
 DROP TABLE IF EXISTS jobs;
 
 -- Two-stage crawl: listing pages -> detail pages via LATERAL crawl_url()
 STREAM (
     WITH job_urls AS (
-        SELECT url, unnest(htmlpath(html.document, 'input#jobs@value[*]')::JSON[]) as job
-        FROM crawl(['https://carrieres.os4techno.com/', 'https://recruit.srilankan.com/'])
-        WHERE status = 200
+        SELECT format('{}jobs/Careers/{}', url, job_id) as job_url
+        FROM (
+            SELECT url, unnest(htmlpath(html.document, 'input#jobs@value[*].id')::VARCHAR[]) as job_id
+            FROM crawl(['https://carrieres.os4techno.com/', 'https://recruit.srilankan.com/'])
+            WHERE status = 200
+        )
     ),
     job_pages AS (
         SELECT c.url, c.html.js['jobs'][0] as job
-        FROM job_urls j, LATERAL crawl_url(format('{}/jobs/Careers/{}', rtrim(j.url,'/'), j.job->>'id')) c
+        FROM job_urls, LATERAL crawl_url(job_url) c
         WHERE c.status = 200 AND c.html.js['jobs'] IS NOT NULL
     )
-    SELECT
-        url,
-        job->>'id' as id,
-        job->>'Posting_Title' as posting_title,
-        job->>'City' as city,
-        job->>'State' as state,
-        job->>'Country' as country,
-        job->>'Industry' as industry,
-        job->>'Job_Type' as job_type,
-        job->>'Work_Experience' as work_experience,
-        (job->>'Remote_Job')::BOOLEAN as remote_job,
-        (job->>'Date_Opened')::DATE as date_opened,
-        job->>'Salary' as salary,
-        job->>'Job_Description' as job_description
+    SELECT url, job->>'id' as id, job->>'Posting_Title' as posting_title,
+           job->>'City' as city, job->>'State' as state, job->>'Country' as country,
+           job->>'Industry' as industry, job->>'Job_Type' as job_type,
+           job->>'Work_Experience' as work_experience,
+           (job->>'Remote_Job')::BOOLEAN as remote_job,
+           (job->>'Date_Opened')::DATE as date_opened,
+           job->>'Salary' as salary, job->>'Job_Description' as job_description
     FROM job_pages
 ) INTO jobs;
 
